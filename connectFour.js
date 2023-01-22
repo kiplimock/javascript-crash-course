ad6d6f44ec25485ba107e340cd0a86ffc92120e3
Cedric Kiplimo <kiplimock@gmail.com>
2023-01-22 18:04:29 +0300

    switching players and chip colour

--- connectFour.js
+++ connectFour.js
@@ -1,20 +1,53 @@
-var td = $('td'); 
+var td = document.querySelectorAll("td");
 
-td.each(function() {
-    $(this).addClass("turnGray")
-})
+td.forEach(element => {
+    element.classList.add("turnGray");
+});
+// function to return the column clicked
+// function columnClicked(elements) {
+//     elements.on("click", function(event) {
+//         console.log(event.target.className === "gray")
+//     })
+//     return elements
+// }
 
-var dropChip = function() {
-    // console.log("Row: " + $(this)[0].closest("tr").rowIndex);
-    // console.log("Col: " + $(this)[0].cellIndex); 
-    var colNum = $(this)[0].cellIndex
-    var colChips = Object.entries(td).filter(x => x[1].cellIndex === colNum);
-    grayChips = colChips.filter(x => x[1].className === 'turnGray');
-    // console.log(grayChips[grayChips.length - 1][1])
-    grayChips[grayChips.length - 1][1].classList.remove("turnGray");
-    grayChips[grayChips.length - 1][1].classList.add("turnBlue");
+// columnClicked(td);
+
+// function to return the css class of the lowest chip in the column
+function dropChip() {
+    var player = "Player 1";
+    td.forEach(element => {
+        
+        element.addEventListener("click", function() {
+            var colNum = 0;
+            var grayChips = [];
+            var colChips = [];
+            console.log(player);
+
+            colNum = $(this)[0].cellIndex;
+            colChips = Object.entries(td).filter(x => x[1].cellIndex === colNum);
+            grayChips = colChips.filter(x => x[1].className === 'turnGray');
+            if (grayChips.length >= 1) {
+                var lowestChip = $(grayChips[grayChips.length - 1][1]);
+                $(lowestChip).removeClass("turnGray");
+                // $(lowestChip).addClass("turnBlue");
+
+                // var nextLowestChip = $(colChips[grayChips.length])[1]
+
+                if (player === "Player 1") {
+                    $(lowestChip).addClass("turnBlue");
+                    player = "Player 2";
+                } else if (player === "Player 2") {
+                    $(lowestChip).addClass("turnRed");
+                    player = "Player 1";
+                }
+            }
+            else {
+                console.log("column full")
+            }
+        }) 
+    })
 }
 
-td.each(function() {
-    $(this).on("click", dropChip)
-})
+
+dropChip();
